f24e448c38fbbe82c9b785674b9308d96804993d
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-18 14:41:51 +0100

    with documentation

--- tools.js
+++ tools.js
@@ -1,5 +1,12 @@
 import { exec } from 'child_process';
 
+/**
+ * getNpmRoot
+ * 
+ * returns the directory of where node_modules is stored on Linux, Mac and Windows
+ * 
+ * const npmRoot = await tools.getNpmRoot();
+ */
 export const getNpmRoot = () => {
 	return new Promise((resolve) => {
 		exec('npm root -g', (error, stdout, stderr) => {
