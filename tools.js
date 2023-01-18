0baa8cdc8a37bc80513fb278d316a23f8922ba01
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-18 14:52:37 +0100

    cleaned up

--- tools.js
+++ tools.js
@@ -10,19 +10,11 @@ import { exec } from 'child_process';
 export const getNpmRoot = () => {
 	return new Promise((resolve) => {
 		exec('npm root -g', (error, stdout, stderr) => {
-			stdout = 'WARN: this is a warning\n' + stdout;
-			// resolve(stdout);
-
-			//remove any warnings that might come with output
 			stdout = stdout.trim('\n');
 			const lines = stdout.split('\n');
-			console.log(lines)
 			if (lines.length > 1) {
 				stdout = lines[lines.length - 1];
 			}
-
-			// stdout = stdout.trim();
-
 			resolve(stdout);
 		});
 	});
